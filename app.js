d541b31ff2086b3f35bcc60aa6b4362b6a397103
niloyKGhosh <niloykumar256@gmail.com>
2020-07-16 11:58:00 -0400

    updated morgan

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const morgan = require('morgan');
+const morgan = require("morgan");
 
 const app = express();
 
-
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(bodyParser.json());
 
-
-app.get("/", (req, res, next) => {
-
-    res.send("Hello World!");
+app.get("/message", (req, res, next) => {
+	res.send("Hello World!");
 });
 
 const port = process.env.PORT || 5500;
+
 app.listen(port, () => {
-    console.log(`The server is listening at ${port}`);
+	console.log(`The server is listening at ${port}`);
 });
